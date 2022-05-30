58c94d1494c2e095bea33ea0df1502038f750621
Cales <86434489+calesj@users.noreply.github.com>
2022-05-30 09:16:21 -0300

    [add] add buscar-curso.php

--- buscar-curso.php
+++ buscar-curso.php
@@ -1,1 +1,17 @@
 <?php
+
+require 'vendor/autoload.php';
+use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
+
+$client = new Client(['verify' => false]);
+$res = $client->request('GET', 'https://www.alura.com.br/cursos-online-programacao/php');
+$html = $res->getBody();
+
+$crawler = new Crawler();
+$crawler->addHtmlContent($html);
+$cursos = $crawler->filter("span.card-curso__nome");
+
+foreach ($cursos as $curso){
+    echo PHP_EOL. $curso->textContent . PHP_EOL;
+}
